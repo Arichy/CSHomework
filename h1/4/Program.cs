2115e0056664e733d9a5f958e40eb909c62396c5
Arichy <452609128@qq.com>
2019-03-01 13:48:19 +0800

    第一次作业

---
+++ h1/4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入第一个整数:");
+            string input1 = Console.ReadLine();
+            int num1 = Convert.ToInt32(input1);
+
+            Console.WriteLine("请输入第二个整数:");
+            string input2 = Console.ReadLine();
+            int num2 = Convert.ToInt32(input2);
+
+            Console.WriteLine("相乘的结果为:");
+            Console.WriteLine(num1*num2);
+        }
+    }
+}
